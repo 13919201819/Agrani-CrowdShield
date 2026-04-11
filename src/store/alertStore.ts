8c7056145f0dc748ae1b27ae14ca1f8970c58394
13919201819 <humaan01012001@gmail.com>
2026-04-11 11:20:01 +0530

    zustand

--- src/store/alertStore.ts
+++ src/store/alertStore.ts
@@ -1,6 +1,17 @@
 import { create } from "zustand";
 
-export const useAlertStore = create((set) => ({
+export interface Alert {
+  location: string;
+  risk: number;
+  density: number;
+}
+
+interface AlertStore {
+  alerts: Alert[];
+  addAlert: (alert: Alert) => void;
+}
+
+export const useAlertStore = create<AlertStore>((set) => ({
   alerts: [],
   addAlert: (alert) =>
     set((state) => ({
